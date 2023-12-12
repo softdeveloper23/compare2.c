f35d28cfdcf53f112993d77a6ca5b5c52e3fad85
unknown <brannongarrett77@gmail.com>
2023-12-12 13:43:58 +0100

    Added strcmp

--- compare2.c
+++ compare2.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "cs50.h"
+#include <string.h>
 
 int main(void)
 {
     char *s = get_string("s: ");
     char *t = get_string("t: ");
 
-    if (s == t)
+    if (strcmp(s, t) == 0)
     {
         printf("Same\n");
     }
