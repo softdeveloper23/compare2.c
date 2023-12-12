a1eb638cf03409934f89f49f1c9f7772e7486259
unknown <brannongarrett77@gmail.com>
2023-12-12 13:50:38 +0100

    Added code

--- compare2.c
+++ compare2.c
@@ -4,10 +4,10 @@
 
 int main(void)
 {
-    char *s = get_string("s: ");
-    char *t = get_string("t: ");
+    char *s = get_string("s: "); // get_string returns a pointer to a string
+    char *t = get_string("t: "); // get_string returns a pointer to a string
 
-    if (strcmp(s, t) == 0)
+    if (strcmp(s, t) == 0) // strcmp returns 0 if the strings are the same
     {
         printf("Same\n");
     }
@@ -15,4 +15,6 @@ int main(void)
     {
         printf("Different\n");
     }
+    printf("s is at address %p\n", s); // %p is a format specifier for a pointer
+    printf("t is at address %p\n", t); // %p is a format specifier for a pointer
 }
