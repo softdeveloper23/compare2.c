a58560bbf269d22197dc9459f62176e8d2a979a4
unknown <brannongarrett77@gmail.com>
2023-12-12 13:41:29 +0100

    Added strings

--- compare2.c
+++ compare2.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <cs50.h>
+#include "cs50.h"
 
 int main(void)
 {
-    int i = get_int("i: ");
-    int j = get_int("j: ");
+    char *s = get_string("s: ");
+    char *t = get_string("t: ");
 
-    if (i == j)
+    if (s == t)
     {
         printf("Same\n");
     }
